1087f9058fd353a217fdff9b0a320acb3d23731c
市川裕太 <yu74ichi69@gmail.com>
2020-11-18 14:00:34 +0900

    header部分をデフォルトから変更

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,18 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="purple lighten-3"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <div class="logo-title">CommandBox</div>
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://twitter.com/icchi_IT"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-icon>mdi-twitter</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -58,3 +38,11 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+.logo-title {
+  font-weight: bold;
+  font-size: x-large;
+  font-family: "Courier New", Courier, monospace;
+}
+</style>

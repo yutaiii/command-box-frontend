8ebf3e58a25005b6ccc670d307ba644cb209a672
市川裕太 <yu74ichi69@gmail.com>
2020-11-19 14:07:39 +0900

    dataから表示内容を取得する様に変更

--- src/components/CommandList.vue
+++ src/components/CommandList.vue
@@ -2,58 +2,13 @@
   <v-container>
 
     <v-row class="text-center">
-      <v-col cols="3">
+      <v-col cols="3" v-for="(item, i) in commandList" :key="i">
         <div class="command-box">
-          <h2 class="box-title">安全にforce pushする</h2>
+          <h2 class="box-title">{{ item.title }}</h2>
           <v-row>
             <v-col cols="10">
               <span class="code">
-                $ git push --force-with-lease [remote-name] [branch-name]
-              </span>
-            </v-col>
-            <v-col cols="2">
-              <v-icon>mdi-content-copy</v-icon>
-            </v-col>
-          </v-row>
-        </div>
-      </v-col>
-      <v-col cols="3">
-        <div class="command-box">
-          <h2 class="box-title">安全にforce pushする</h2>
-          <v-row>
-            <v-col cols="10">
-              <span class="code">
-                $ git push --force-with-lease [remote-name] [branch-name]
-              </span>
-            </v-col>
-            <v-col cols="2">
-              <v-icon>mdi-content-copy</v-icon>
-            </v-col>
-          </v-row>
-        </div>
-      </v-col>
-      <v-col cols="3">
-        <div class="command-box">
-          <h2 class="box-title">安全にforce pushする</h2>
-          <v-row>
-            <v-col cols="10">
-              <span class="code">
-                $ git push --force-with-lease [remote-name] [branch-name]
-              </span>
-            </v-col>
-            <v-col cols="2">
-              <v-icon>mdi-content-copy</v-icon>
-            </v-col>
-          </v-row>
-        </div>
-      </v-col>
-      <v-col cols="3">
-        <div class="command-box">
-          <h2 class="box-title">安全にforce pushする</h2>
-          <v-row>
-            <v-col cols="10">
-              <span class="code">
-                $ git push --force-with-lease [remote-name] [branch-name]
+                $ {{ item.text }}
               </span>
             </v-col>
             <v-col cols="2">
@@ -72,6 +27,24 @@
     name: 'CommandList',
 
     data: () => ({
+      commandList: [
+        {
+          title: '安全にforce pushする',
+          text: 'git push --force-with-lease [remote-name] [branch-name]',
+        },
+        {
+          title: '安全にforce pushする',
+          text: 'git push --force-with-lease [remote-name] [branch-name]',
+        },
+        {
+          title: '安全にforce pushする',
+          text: 'git push --force-with-lease [remote-name] [branch-name]',
+        },
+        {
+          title: '安全にforce pushする',
+          text: 'git push --force-with-lease [remote-name] [branch-name]',
+        }
+      ],
     }),
   }
 </script>

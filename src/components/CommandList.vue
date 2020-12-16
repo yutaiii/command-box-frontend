f55b844eaf0cd360399317a385a5e09e495db333
市川裕太 <yu74ichi69@gmail.com>
2020-12-16 14:00:02 +0900

    [wip]4列表示にしたかったがうまくいかなかったので、一旦1列でコミット

--- src/components/CommandList.vue
+++ src/components/CommandList.vue
@@ -1,9 +1,81 @@
 <template>
   <v-container>
+  <div class="container-wapper">
 
-    <v-row>
-      <v-col cols="3" v-for="(item, i) in commandList" :key="i">
-        <div class="command-box">
+    <div class="container">
+      <div v-for="(item, i) in commandList" :key="i">
+        <div class="command-box" v-if="i % 4 === 0">
+          <h2 class="box-title">{{ item.Title }}</h2>
+          <v-row>
+            <v-col cols="10">
+              <span class="code">
+                $ {{ item.Text }}
+              </span>
+            </v-col>
+            <v-col cols="2">
+              <v-icon @click="copyToClipboard(item.Text)">mdi-content-copy</v-icon>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div v-for="(item, i) in commandList" :key="i">
+        <div class="command-box" v-if="i % 4 === 1">
+          <h2 class="box-title">{{ item.Title }}</h2>
+          <v-row>
+            <v-col cols="10">
+              <span class="code">
+                $ {{ item.Text }}
+              </span>
+            </v-col>
+            <v-col cols="2">
+              <v-icon @click="copyToClipboard(item.Text)">mdi-content-copy</v-icon>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div v-for="(item, i) in commandList" :key="i">
+        <div class="command-box" v-if="i % 4 === 2">
+          <h2 class="box-title">{{ item.Title }}</h2>
+          <v-row>
+            <v-col cols="10">
+              <span class="code">
+                $ {{ item.Text }}
+              </span>
+            </v-col>
+            <v-col cols="2">
+              <v-icon @click="copyToClipboard(item.Text)">mdi-content-copy</v-icon>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div v-for="(item, i) in commandList" :key="i">
+        <div class="command-box" v-if="i % 4 === 3">
+          <h2 class="box-title">{{ item.Title }}</h2>
+          <v-row>
+            <v-col cols="10">
+              <span class="code">
+                $ {{ item.Text }}
+              </span>
+            </v-col>
+            <v-col cols="2">
+              <v-icon @click="copyToClipboard(item.Text)">mdi-content-copy</v-icon>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+    </div>
+    <!-- <v-row class="test">
+      <v-col cols="12" v-for="(item, i) in commandList" :key="i">
+        <div class="command-box" v-if="i % 3 === 0">
           <h2 class="box-title">{{ item.Title }}</h2>
           <v-row>
             <v-col cols="10">
@@ -19,6 +91,43 @@
       </v-col>
     </v-row>
 
+    <v-row class="test">
+      <v-col cols="12" v-for="(item, i) in commandList" :key="i">
+        <div class="command-box" v-show="i % 3 === 1">
+          <h2 class="box-title">{{ item.Title }}</h2>
+          <v-row>
+            <v-col cols="10">
+              <span class="code">
+                $ {{ item.Text }}
+              </span>
+            </v-col>
+            <v-col cols="2">
+              <v-icon @click="copyToClipboard(item.Text)">mdi-content-copy</v-icon>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="4" v-for="(item, i) in commandList" :key="i">
+        <div class="command-box" v-if="i % 3 === 2">
+          <h2 class="box-title">{{ item.Title }}</h2>
+          <v-row>
+            <v-col cols="10">
+              <span class="code">
+                $ {{ item.Text }}
+              </span>
+            </v-col>
+            <v-col cols="2">
+              <v-icon @click="copyToClipboard(item.Text)">mdi-content-copy</v-icon>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+    </v-row> -->
+
+  </div>
   </v-container>
 </template>
 
@@ -60,6 +169,16 @@ import axios from 'axios'
 .command-box {
   background-color: #f0f0f0;
   padding: 10px;
+  margin-top: 10px;
+}
+
+.container {
+  /* width: 25%; */
+  float: left;
+}
+
+.container-wapper {
+  /* width: 1200px; */
 }
 
 .box-title {
@@ -75,9 +194,5 @@ import axios from 'axios'
   float: left;
   text-align: left;
   word-break: break-all ;
-}
-
-.test {
-  background-color: aqua;
 }
 </style>
